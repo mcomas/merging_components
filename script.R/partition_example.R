5959aada057295e9cdab5b563866401b0993a313
mcomas <marc@macmini.local>
2015-12-07 21:27:35 +0100

    The example has been modified

--- script.R/partition_example.R
+++ script.R/partition_example.R
@@ -1,35 +1,31 @@
-if(!exists('SEED')) SEED = 70
-if(!exists('MAXOVERLAP')) MAXOVERLAP = 0.01
-if(!exists('K0')) K0 = 3
-if(!exists('DIM')) DIM = 2
-
-if(!exists('NDATA')) NDATA = 500
-if(!exists('Kf')) Kf = 6  ## Clusters utilitzats en el darrer pas
-
-# if(!exists('NSIM')) NSIM = 100
-# if(!exists('OFILE')) OFILE = 'data/sim-01.RData'
-
-# library(plyr)
-# library(reshape2)
-# library(ggplot2)
-# library(stringr)
-# library(Hmisc)
-# library(abind)
-
-set.seed(SEED)
+NDATA = 500
+set.seed(1)
 
 library(MixSim)
-ms = MixSim(MaxOmega=MAXOVERLAP, K=K0, p=DIM, PiLow=0.01, sph=TRUE)
+SEP = 40
+ms = list(Pi = rep(1/6, 6),
+     Mu = array(c(0,  0,SEP,SEP,SEP,   0,
+                  0,  0,SEP,SEP,  0, SEP), c(6,2)),
+     S  = array(c(50, 0,
+                  0, 5,
+                  5, 0,
+                  0, 50), c(2,2,6)))
+       
 SIM = simdataset(n=NDATA, Pi=ms$Pi, Mu=ms$Mu, S=ms$S)
 
-df = data.frame(100*SIM$X)
+df = data.frame(SIM$X)
 
+plot(df)
 library(Rmixmod)
-mixt = mixmodCluster(df, nbCluster=Kf, strategy=mixmodStrategy(nbTry = 10, seed = 1))
-
-P  = round(mixt@bestResult@parameters@proportions, 2)
-M  = round(mixt@bestResult@parameters@mean, 2)
-S = lapply(mixt@bestResult@parameters@variance, round, 2)
+mixt = mixmodCluster(df, nbCluster=6, strategy=mixmodStrategy(nbTry = 1, seed = 1),
+                     models = mixmodGaussianModel(listModels="Gaussian_pk_Lk_Ck"))
+ord = c(1,6,4,2,5,3)
+# P  = round(mixt@bestResult@parameters@proportions, 2)
+P = ms$Pi
+# M  = round(mixt@bestResult@parameters@mean, 2)
+M = ms$Mu[ord,]
+# S = lapply(mixt@bestResult@parameters@variance, round, 2)
+S = lapply(ord, function(i) ms$S[,,i])
 
 library(stringr)
 library(Hmisc)
@@ -38,8 +34,8 @@ for(i in 1:4){
   cat('\\[\n')
   cat('\\begin{array}{l@{\\hskip 0.1in}l@{\\hskip 0.1in}c }\n')
   
-  cat(sprintf("\\hat{\\pi}_%d = %s, & \\hat{\\m\\mu}_%d = \\left(%s\\right), & \\hat{\\m\\Sigma}_%d = \\left(\n%s\\right), \\\\ & &\\\\ \n",
-              i, paste(P[i], collapse = ','),
+  cat(sprintf("\\pi_%d = \\frac{1}{6}, & \\m\\mu_%d = \\left(%s\\right), & \\m\\Sigma_%d = \\left(\n%s\\right), \\\\ & &\\\\ \n",
+              i, 
               i, paste(M[i,], collapse = ','),
               i, str_replace_all(latexTabular(S[[i]], headings = NULL, helvetica = FALSE), 'tabular', 'array')))
   
@@ -49,8 +45,8 @@ for(i in 1:4){
 cat('\\[\n')
 cat('\\begin{array}{l@{\\hskip 0.1in}l@{\\hskip 0.1in}c }\n')
 i = 5
-cat(sprintf("\\hat{\\pi}_%d = %s, & \\hat{\\m\\mu}_%d = \\left(%s\\right), & \\hat{\\m\\Sigma}_%d = \\left(\n%s\\right) \\text{and} \\\\ & &\\\\ \n",
-            i, paste(P[i], collapse = ','),
+cat(sprintf("\\pi_%d = \\frac{1}{6}, & \\m\\mu_%d = \\left(%s\\right), & \\m\\Sigma_%d = \\left(\n%s\\right) \\text{and} \\\\ & &\\\\ \n",
+            i, 
             i, paste(M[i,], collapse = ','),
             i, str_replace_all(latexTabular(S[[i]], headings = NULL, helvetica = FALSE), 'tabular', 'array')))
 
@@ -60,8 +56,8 @@ cat('\\]\n')
 cat('\\[\n')
 cat('\\begin{array}{l@{\\hskip 0.1in}l@{\\hskip 0.1in}c }\n')
 i = 6
-cat(sprintf("\\hat{\\pi}_%d = %s, & \\hat{\\m\\mu}_%d = \\left(%s\\right), & \\hat{\\m\\Sigma}_%d = \\left(\n%s\\right). \\\\ & &\\\\ \n",
-            i, paste(P[i], collapse = ','),
+cat(sprintf("\\pi_%d = \\frac{1}{6}, & \\m\\mu_%d = \\left(%s\\right), & \\m\\Sigma_%d = \\left(\n%s\\right). \\\\ & &\\\\ \n",
+            i, 
             i, paste(M[i,], collapse = ','),
             i, str_replace_all(latexTabular(S[[i]], headings = NULL, helvetica = FALSE), 'tabular', 'array')))
 
@@ -108,11 +104,15 @@ df$id = sapply(ii, function(i) sprintf('{%s}',paste(i, collapse=',')) )
 p.all <- ggplot() + 
   stat_contour(data=cm, aes(x=X1, y=X2, z=z), alpha=0.1) +
   geom_point(data=df, aes(x=X1, y=X2), alpha=0.8, size=1) +
-  stat_contour(data=CN, aes(x=X1, y=X2, z=z), alpha=0.6) + 
-  facet_wrap(~id, nrow=1) + theme_bw() + theme(legend.position="none") 
+  stat_contour(data=CN, aes(x=X1, y=X2, z=z), alpha=0.6, col='blue') + 
+  facet_wrap(~id, nrow=1) + theme_bw() + theme(legend.position="none") +
+  scale_x_continuous(breaks=c(0, 20, 40)) + scale_y_continuous(breaks=c(0, 20, 40))
 ggsave(p.all, filename = 'figures/partition-example-part6.pdf', width = 10, height=2.5)
 
-partition = list(c(1, 3, 6), c(2, 5), 4)
+HP = get_hierarchical_partition(mixt@bestResult@proba[,ord], omega = 'cnst', lambda = 'entr')
+HP = lapply(HP, lapply, function(v) ord[v])
+
+partition = HP[[3]]
 names(partition) = sapply(partition, function(v) paste(sort(v), collapse=','))
 
 CN3 = lapply(partition, function(part){
@@ -137,7 +137,8 @@ p.cn3 <- ggplot() +
   stat_contour(data=cm, aes(x=X1, y=X2, z=z), alpha=0.1) +
   geom_point(data=df, aes(x=X1, y=X2), alpha=0.8, size=1) +
   stat_contour(data=CN3, aes(x=X1, y=X2, z=z), alpha=0.8) + 
-  facet_wrap(~id, nrow=1) + theme_bw() + theme(legend.position="none")
+  facet_wrap(~id, nrow=1) + theme_bw() + theme(legend.position="none") +
+  scale_x_continuous(breaks=c(0, 20, 40)) + scale_y_continuous(breaks=c(0, 20, 40))
 ggsave(p.cn3, filename = 'figures/partition-example-part3a.pdf', width = 5, height=2.5)
 
 partition = list(c(1, 2, 3), 4, c(5, 6))
@@ -156,10 +157,11 @@ p.cn3b <- ggplot() +
   stat_contour(data=cm, aes(x=X1, y=X2, z=z), alpha=0.1) +
   geom_point(data=df, aes(x=X1, y=X2), alpha=0.8, size=1) +
   stat_contour(data=CN3b, aes(x=X1, y=X2, z=z), alpha=0.8) + 
-  facet_wrap(~id, nrow=1) + theme_bw() + theme(legend.position="none")
+  facet_wrap(~id, nrow=1) + theme_bw() + theme(legend.position="none") +
+  scale_x_continuous(breaks=c(0, 20, 40)) + scale_y_continuous(breaks=c(0, 20, 40))
 ggsave(p.cn3b, filename = 'figures/partition-example-part3b.pdf', width = 5, height=2.5)
 
-partition = list(c(1, 6), 2, 3, 4, 5)
+partition = HP[[5]]
 
 CN5 = lapply(partition, function(part){
   cn = expand.grid(X1 = xlimits, X2 = ylimits)
@@ -175,10 +177,11 @@ p.cn5 <- ggplot() +
   stat_contour(data=cm, aes(x=X1, y=X2, z=z), alpha=0.1) +
   geom_point(data=df, aes(x=X1, y=X2), alpha=0.8, size=1) +
   stat_contour(data=CN5, aes(x=X1, y=X2, z=z), alpha=0.8) + 
-  facet_wrap(~id, nrow=1) + theme_bw() + theme(legend.position="none")
+  facet_wrap(~id, nrow=1) + theme_bw() + theme(legend.position="none") +
+  scale_x_continuous(breaks=c(0, 20, 40)) + scale_y_continuous(breaks=c(0, 20, 40))
 ggsave(p.cn5, filename = 'figures/partition-example-part5.pdf', width = 8.3, height=2.5)
 
-partition = list(c(1, 3, 6), 2, 4, 5)
+partition = HP[[4]]
 
 CN4 = lapply(partition, function(part){
   cn = expand.grid(X1 = xlimits, X2 = ylimits)
@@ -193,11 +196,12 @@ df$id = sapply(ii, function(i) sprintf('{%s}',paste(partition[[i]], collapse=','
 p.cn4 <- ggplot() + 
   stat_contour(data=cm, aes(x=X1, y=X2, z=z), alpha=0.1) +
   geom_point(data=df, aes(x=X1, y=X2), alpha=0.8, size=1) +
-  stat_contour(data=CN4, aes(x=X1, y=X2, col=id, z=z), alpha=0.8) + 
-  facet_wrap(~id, nrow=1) + theme_bw() + theme(legend.position="none")
+  stat_contour(data=CN4, aes(x=X1, y=X2, z=z), col='blue', alpha=0.8) + 
+  facet_wrap(~id, nrow=1) + theme_bw() + theme(legend.position="none") +
+  scale_x_continuous(breaks=c(0, 20, 40)) + scale_y_continuous(breaks=c(0, 20, 40))
 ggsave(p.cn4, filename = 'figures/partition-example-part4.pdf', width = 6.6, height=2.5)
 
-partition = list(c(1, 3, 6), c(2, 4, 5))
+partition = HP[[2]]
 
 CN2 = lapply(partition, function(part){
   cn = expand.grid(X1 = xlimits, X2 = ylimits)
@@ -213,10 +217,11 @@ p.cn2 <- ggplot() +
   stat_contour(data=cm, aes(x=X1, y=X2, z=z), alpha=0.1) +
   geom_point(data=df, aes(x=X1, y=X2), alpha=0.8, size=1) +
   stat_contour(data=CN2, aes(x=X1, y=X2, z=z), alpha=0.8) + 
-  facet_wrap(~id, nrow=1) + theme_bw() + theme(legend.position="none")
+  facet_wrap(~id, nrow=1) + theme_bw() + theme(legend.position="none") +
+  scale_x_continuous(breaks=c(0, 20, 40)) + scale_y_continuous(breaks=c(0, 20, 40))
 ggsave(p.cn2, filename = 'figures/partition-example-part2.pdf', width = 3.3, height=2.5)
 
-partition = list(c(1, 2, 3, 4, 5, 6))
+partition = HP[[1]]
 
 CN1 = lapply(partition, function(part){
   cn = expand.grid(X1 = xlimits, X2 = ylimits)
@@ -232,5 +237,6 @@ p.cn1 <- ggplot() +
   stat_contour(data=cm, aes(x=X1, y=X2, z=z), alpha=0.1) +
   geom_point(data=df, aes(x=X1, y=X2), alpha=0.8, size=1) +
   stat_contour(data=CN1, aes(x=X1, y=X2, z=z), alpha=0.8) + 
-  facet_wrap(~id, nrow=1) + theme_bw() + theme(legend.position="none")
+  facet_wrap(~id, nrow=1) + theme_bw() + theme(legend.position="none") +
+  scale_x_continuous(breaks=c(0, 20, 40)) + scale_y_continuous(breaks=c(0, 20, 40))
 ggsave(p.cn1, filename = 'figures/partition-example-part1.pdf', width = 2, height=2.5)
